435817f3eb35b443072627bcd2ad465c616f70b3
thimakulani <thimakulani@gmail.com>
2021-09-27 12:37:26 +0200

    Add project files.

---
+++ kota/kota/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kota.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+}
